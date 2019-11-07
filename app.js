d438c406c812456a73376dd9e934942789e9759e
Thanapech Tunwanarutsakul <thanapech.tunwanarutsakul@thomsonreuters.com>
2019-11-07 18:49:50 +0700

    update

--- app.js
+++ app.js
@@ -12,25 +12,28 @@ app.post("/webhook", (req, res) => {
 });
 app.listen(port);
 
+function randomNumber() { 
+  let number = Math.floor(Math.random() * Math.floor(9999) + 1000).toString().split("")
+  return number.join(" ");
+}
+
 function reply(reply_token) {
   let headers = {
     "Content-Type": "application/json",
     Authorization:
       "Bearer {yFxy1RzCMiuvV9MhDF94+J0RLca42YuLGBCQwXQmfj8yCpHvwPwLdtbJG6uZAM9a7Mga2dzrP94tkBJurfdWyxfIRcBMOtJfF9LwTLuhEBi0vh17PMwy3XvddbQrSbRZgg4YfuTpbCQ5GqidMJJrUwdB04t89/1O/w1cDnyilFU=}"
   };
+
   let body = JSON.stringify({
     replyToken: reply_token,
     messages: [
       {
         type: "text",
-        text: Math.floor(Math.random() * Math.floor(10000))
-          .toString()
-          .split("")
-          .join(" ")
+        text: randomNumber()
       }
     ]
   });
-  request.post(
+    request.post(
     {
       url: "https://api.line.me/v2/bot/message/reply",
       headers: headers,
