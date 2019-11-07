a61e6ccaa3d4ffc2f09777cfbedb8cc39b0ca274
Thanapech Tunwanarutsakul <thanapech.tunwanarutsakul@thomsonreuters.com>
2019-11-07 16:55:59 +0700

    first massage bot

--- app.js
+++ app.js
@@ -1,5 +1,45 @@
-const express = require("express");
+import express from "express";
+import bodyParser from "body-parser";
+import request from "request";
+
 const app = express();
 const port = process.env.PORT || 4000;
-app.post("/webhook", (req, res) => res.sendStatus(200));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.post("/webhook", (req, res) => {
+  let reply_token = req.body.events[0].replyToken;
+  reply(reply_token);
+  res.sendStatus(200);
+});
 app.listen(port);
+function reply(reply_token) {
+  let headers = {
+    "Content-Type": "application/json",
+    Authorization:
+      "Bearer {rIh3Ww9u8DZErUehjzpLvSMtyNRkhPu5cKrcJvqIrOd7ZtGUud5zajubcDEc9Sxz7Mga2dzrP94tkBJurfdWyxfIRcBMOtJfF9LwTLuhEBgEMGc5uFsQ5H+oWEmfujFhytaNVWfJjkjm+0DnOIX2YgdB04t89/1O/w1cDnyilFU=}"
+  };
+  let body = JSON.stringify({
+    replyToken: reply_token,
+    messages: [
+      {
+        type: "text",
+        text: "Hello"
+      },
+      {
+        type: "text",
+        text: "How are you?"
+      }
+    ]
+  });
+  request.post(
+    {
+      url: "https://api.line.me/v2/bot/message/reply",
+      headers: headers,
+      body: body
+    },
+    (err, res, body) => {
+      console.log("status = " + res.statusCode);
+    }
+  );
+}
+s;
