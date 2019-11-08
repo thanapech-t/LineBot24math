4ecf0a62c81b49eac84a8bad9da68159681a3059
Thanapech Tunwanarutsakul <thanapech.tunwanarutsakul@thomsonreuters.com>
2019-11-08 16:57:37 +0700

    update message

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.post("/webhook", (req, res) => {
   let reply_token = req.body.events[0].replyToken;
-  reply(reply_token);
+  let msg = req.body.events[0].message.text;
+  reply(reply_token, msg);
   res.sendStatus(200);
 });
 
@@ -22,7 +23,7 @@ function randomNumber() {
   return arr.join(" ");
 }
 
-function reply(reply_token) {
+function reply(reply_token, msg) {
   let headers = {
     "Content-Type": "application/json",
     Authorization:
@@ -34,8 +35,7 @@ function reply(reply_token) {
     messages: [
       {
         type: "text",
-        text:
-          reply_token === "random !" ? randomNumber() : "please click button"
+        text: msg === "random !" ? randomNumber() : "please click button"
       }
     ]
   });
